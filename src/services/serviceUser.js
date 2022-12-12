ec00bc2c9c7339b26abc23b3e3e52b77eaea7156
Eri Schon <erischon@gmail.com>
2022-12-12 08:25:00 +0100

    login user service in progress

---
+++ src/services/serviceUser.js
@@ -0,0 +1,24 @@
+import axios from "../api/axios";
+
+const LOGIN_URL = "/user/login";
+
+const payload = {
+  email: "tony@stark.com",
+  password: "password123",
+};
+
+/**
+ * Login user and get the JWT token
+ * @param {payload}
+ * @returns {}
+ */
+const userLogin = async (payload) => {
+  try {
+    const response = await axios.post(LOGIN_URL, JSON.stringify(payload), {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    });
+    console.log(JSON.stringify(response?.data));
+    const accessToken = response?.data?.accessToken;
+  } catch (err) {}
+};
