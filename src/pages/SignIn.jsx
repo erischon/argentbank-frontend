210f13ab09f902d7ac2138c885ad90de44955ed7
Eri Schon <erischon@gmail.com>
2023-01-03 09:22:43 +0100

    user profile service creation

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom";
 
-import { userLogin, userSignup } from "../services/serviceUser";
-
 const SignIn = () => {
   return (
     <main className="main bg-dark">
--- src/services/serviceUser.js
+++ src/services/serviceUser.js
@@ -2,23 +2,25 @@ import axios from "../api/axios";
 
 const LOGIN_URL = "/user/login";
 const SIGNUP_URL = "/user/signup";
+const USER_PROFILE_URL = "/user/profile";
 
 /**
- * Login user and get the JWT token
- * @param {payload}
+ * User Login and Get the JWT Token
+ * @param {{email: string, password: string}} payload
  * @returns {}
  */
 export const userLogin = async (payload) => {
-  // payload = {
-  //   email: "tony@stark.com",
-  //   password: "password123",
-  // };
+  payload = {
+    email: "tony@stark.com",
+    password: "password123",
+  };
 
   try {
     const response = await axios.post(LOGIN_URL, JSON.stringify(payload), {
       headers: { "Content-Type": "application/json" },
     });
     console.log("======userLogin", JSON.stringify(response?.data?.body?.token));
+    return response?.data?.body?.token;
   } catch (err) {
     console.log("User Login Service Error :", err);
   }
@@ -26,17 +28,10 @@ export const userLogin = async (payload) => {
 
 /**
  * User Signup
- * @param {payload}
+ * @param {string} token
  * @returns {}
  */
 export const userSignup = async (payload) => {
-  // payload = {
-  //   email: "toto@gmail.com",
-  //   password: "password123",
-  //   firstName: "toto",
-  //   lastName: "litoto",
-  // };
-
   try {
     const response = await axios.post(SIGNUP_URL, JSON.stringify(payload), {
       headers: { "Content-Type": "application/json" },
@@ -44,5 +39,57 @@ export const userSignup = async (payload) => {
     console.log("======userSignup", JSON.stringify(response?.data.status));
   } catch (err) {
     console.log("User Signup Service Error :", err);
+  }
+};
+
+/**
+ * Get User Profile
+ * @param {string} token
+ * @returns {}
+ */
+export const getUserProfile = async (token) => {
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOGVlZjMyNWM3M2JkNTIwODg5ZGU2MSIsImlhdCI6MTY3MjcyODUwMCwiZXhwIjoxNjcyODE0OTAwfQ.yYf8fhhVceChPiOG_z84TSpZMxJymoAGFbzJVklBbzs";
+  const data = {
+    key: "value",
+  };
+
+  try {
+    const response = await axios.post(USER_PROFILE_URL, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    console.log("======userProfile", JSON.stringify(response?.data));
+  } catch (err) {
+    console.log("User Profile Service Error :", err);
+  }
+};
+
+/**
+ * Updating User Profile
+ * @param {string} token
+ * @param {{firstName: string, lastName: string}} payload
+ * @returns {}
+ */
+export const updateUserProfile = async (token, payload) => {
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOGVlZjMyNWM3M2JkNTIwODg5ZGU2MSIsImlhdCI6MTY3MjcyODUwMCwiZXhwIjoxNjcyODE0OTAwfQ.yYf8fhhVceChPiOG_z84TSpZMxJymoAGFbzJVklBbzs";
+  payload = {
+    firstName: "value 1",
+    lastName: "value 2",
+  };
+
+  try {
+    const response = await axios.put(USER_PROFILE_URL, payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(
+      "======updateUserProfile",
+      JSON.stringify(response?.data.status)
+    );
+  } catch (err) {
+    console.log("Update User Profile Service Error :", err);
   }
 };
