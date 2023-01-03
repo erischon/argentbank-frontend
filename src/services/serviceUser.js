7879c0214dad38be5435ad5f78aeb8d76f231f18
Eri Schon <erischon@gmail.com>
2023-01-03 07:39:41 +0100

    user signup service

--- src/services/serviceUser.js
+++ src/services/serviceUser.js
@@ -1,26 +1,48 @@
 import axios from "../api/axios";
 
 const LOGIN_URL = "/user/login";
-
-const payload = {
-  email: "tony@stark.com",
-  password: "password123",
-};
+const SIGNUP_URL = "/user/signup";
 
 /**
  * Login user and get the JWT token
  * @param {payload}
  * @returns {}
  */
-const userLogin = async (payload) => {
+export const userLogin = async (payload) => {
+  // payload = {
+  //   email: "tony@stark.com",
+  //   password: "password123",
+  // };
+
   try {
     const response = await axios.post(LOGIN_URL, JSON.stringify(payload), {
       headers: { "Content-Type": "application/json" },
-      withCredentials: true,
     });
-    console.log(JSON.stringify(response?.data));
-    const accessToken = response?.data?.accessToken;
-  } catch (err) {}
+    console.log("======userLogin", JSON.stringify(response?.data?.body?.token));
+  } catch (err) {
+    console.log("User Login Service Error :", err);
+  }
 };
 
-export default userLogin;
+/**
+ * User Signup
+ * @param {payload}
+ * @returns {}
+ */
+export const userSignup = async (payload) => {
+  // payload = {
+  //   email: "toto@gmail.com",
+  //   password: "password123",
+  //   firstName: "toto",
+  //   lastName: "litoto",
+  // };
+
+  try {
+    const response = await axios.post(SIGNUP_URL, JSON.stringify(payload), {
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log("======userSignup", JSON.stringify(response?.data.status));
+  } catch (err) {
+    console.log("User Signup Service Error :", err);
+  }
+};
