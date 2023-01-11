6a537e2b5fec3c12f00050f1be767874fd3a80db
Eri Schon <erischon@gmail.com>
2023-01-11 10:05:16 +0100

    setup Redux Toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./assets/css/main.css";
 
@@ -10,6 +11,7 @@ import Layout from "./components/layouts/Layout";
 import SignIn from "./pages/SignIn";
 import User from "./pages/User";
 import { AuthProvider } from "./context/AuthProvider";
+import { store } from "./app/store";
 
 const router = createBrowserRouter([
   {
@@ -35,8 +37,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </Provider>
   </React.StrictMode>
 );
