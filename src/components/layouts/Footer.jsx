c554c85383fa7f071fd0bc1513734d4b27e5fe08
Eri Schon <erischon@gmail.com>
2023-02-06 09:42:20 +0100

    Update Footer.jsx

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer-text">Copyright 2020 Argent Bank</p>
+      <p className="footer-text">Copyright 2020-2023 Argent Bank</p>
     </footer>
   );
 };
